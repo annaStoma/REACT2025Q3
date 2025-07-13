75c365657a459859a698ff2d54d0a01baf92997f
Hanna <anna.stoma@astorsoft.ru>
2025-07-13 23:12:02 +0400

    Updates

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/REACT2025Q3/',
-  plugins: [react()],
-});
